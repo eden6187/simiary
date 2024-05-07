ae54ade84863f9c36fc298c325ce6a9982ae59f4
eden6187 <ngolo6187@gmail.com>
2024-05-07 20:47:12 +0900

    test ci

--- src/test/java/com/eden6187/simiary/SimiaryApplicationTests.java
+++ src/test/java/com/eden6187/simiary/SimiaryApplicationTests.java
@@ -3,11 +3,18 @@ package com.eden6187.simiary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SimiaryApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test(){
+        assertThat(true).isFalse();
     }
 
 }
