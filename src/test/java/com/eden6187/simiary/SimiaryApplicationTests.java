71c132a2ac4547c55551882058569eab878ce074
eden6187 <ngolo6187@gmail.com>
2024-05-07 20:30:46 +0900

    first commit

---
+++ src/test/java/com/eden6187/simiary/SimiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden6187.simiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
